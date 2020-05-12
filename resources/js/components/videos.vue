d64de23bdb646bac3c14499fd61b2ee05ad1df10
Gergely Olah <olah.gergely@pixeloid.hu>
2020-05-12 21:32:38 +0200

    video box now dynamic

--- resources/js/components/videos.vue
+++ resources/js/components/videos.vue
@@ -6,60 +6,36 @@
             <div class="video-wrapper">
                 <div class="columns">
                     <div class="column is-9 first">
-                        <a class="video-item-button" :href="'https://youtube.com/'">
+                        <a class="video-item-button" :href="'https://youtube.com/watch?v=' + videos[0].youtube_id ">
                             <div class="video-item">
+
                                 <div class="video-item__inner has-text-centered">
+                                <img :src="'https://img.youtube.com/vi/' + videos[0].youtube_id + '/mqdefault.jpg'" alt="">
                                     <img class="play-button" src="/assets/svg/button-video.svg" alt="">
                                 </div>
                             </div>
-                            <p>Kovács Petike influencer makett meséje</p>
+                            <p>{{ videos[0].caption }}</p>
                         </a>
                     </div>
                     <div class="column">
-                        <a class="video-item-button" :href="'https://youtube.com/'">
+
+          
+
+                        <a class="video-item-button" v-for="{ id, caption, youtube_id }, index in videos"
+                v-if="index > '0'" :href="'https://youtube.com/watch?v=' + youtube_id ">
                             <div class="video-item">
+
                                 <div class="video-item__inner has-text-centered">
+                                    <img :src="'https://img.youtube.com/vi/' + youtube_id + '/mqdefault.jpg'" alt="">
                                     <img class="play-button" src="/assets/svg/button-video.svg" alt="">
                                 </div>
                             </div>
-                            <p>Kovács Petike</p>
-                        </a>
-                        <a class="video-item-button" :href="'https://youtube.com/'">
-                            <div class="video-item">
-                                <div class="video-item__inner has-text-centered">
-                                    <img class="play-button" src="/assets/svg/button-video.svg" alt="">
-                                </div>
-                            </div>
-                            <p>Kovács Petike</p>
-                        </a>
-                        <a class="video-item-button" :href="'https://youtube.com/'">
-                            <div class="video-item">
-                                <div class="video-item__inner has-text-centered">
-                                    <img class="play-button" src="/assets/svg/button-video.svg" alt="">
-                                </div>
-                            </div>
-                            <p>Kovács Petike</p>
+                            <p>{{ caption }}</p>
                         </a>
                     </div>
                 </div>
             </div>
 
-            <!--
-            <div class="video-wrapper columns" v-if="videos">
-                <div class="video-item column"
-                v-for="{ id, caption, youtube_id } in videos"
-                :class="{ 'active': videos.length === 0 }">
-                    <div class="video-item__inner">
-                        <a :href="'https://youtube.com/watch?v=' + youtube_id ">
-                            <img :src="'https://img.youtube.com/vi/' + youtube_id + '/mqdefault.jpg'" alt="">
-                            <p>
-                                {{ caption }}
-                            </p>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            -->
         </div>
     </div>
 </template>
