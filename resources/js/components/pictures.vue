bf03485ae731671d3a3cdc53c3c66e384dc2f2f7
Revai Nandor <revnandi@gmail.com>
2020-05-14 18:52:00 +0200

    swiper init

--- resources/js/components/pictures.vue
+++ resources/js/components/pictures.vue
@@ -5,11 +5,11 @@
 
             <div class="picture-wrapper">
                 <div class="columns is-multiline">
-                    <div v-for="{caption, picture} in uploads" class="column is-one-quarter">
+                    <div v-for="({caption, picture}, index) in uploads" :key="index" class="column is-one-quarter">
                         <div class="picture">
                             <div class="picture__inner">
                                 <div class="picture-center has-text-centered">
-                                    <img v-bind:src="'storage/' + picture"  alt="">
+                                    <img v-bind:src="`storage${picture}`"  alt="">
                                 </div>
                             </div>
                         </div>
@@ -17,7 +17,20 @@
                     </div>
                 </div>
             </div>
-            <br>
+            <div class="modal">
+                <div class="modal-background"></div>
+                    <div class="modal-content">
+                        <swiper class="swiper" :options="swiperOption">
+                            <swiper-slide v-for="({caption, picture}, index) in uploads" :key="index" class="column is-one-quarter">
+                                <img :data-src="`/storage/${picture}`" class="swiper-lazy">
+                                <div class="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+                            </swiper-slide>
+                            <div class="swiper-pagination" slot="pagination"></div>
+                            <div class="swiper-button-next swiper-button-white" slot="button-next"></div>
+                            <div class="swiper-button-prev swiper-button-white" slot="button-prev"></div>
+                        </swiper>
+                    </div>
+            </div>
         </div>
     </div>
 </template>
@@ -25,6 +38,9 @@
 
 <script>
 import axios from 'axios';
+
+import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
 
 const getUploads = (callback) => {
 
@@ -38,14 +54,33 @@ const getUploads = (callback) => {
 };
 
 export default {
+    components: {
+        Swiper,
+        SwiperSlide
+    },
+    directives: {
+        swiper: directive
+    },
     data() {
         return {
             uploads: null,
             error: null,
+            swiperOption: {
+                lazy: true,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true
+                },
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev'
+                }
+            }
         };
     },
     mounted () {
         getUploads((err, data) => {
+            console.log(data)
             this.setData(err, data);
         });
     },
