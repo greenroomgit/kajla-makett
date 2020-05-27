5db35a39181ee61b917c6a1356557e30e81a7b89
Gergo Olah <gergo.olah@possible.com>
2020-05-27 17:52:51 +0200

    og test

--- resources/js/components/pages/home.vue
+++ resources/js/components/pages/home.vue
@@ -88,8 +88,19 @@ export default {
     metaInfo: {
       // if no subcomponents specify a metaInfo.title, this title will be used
       // all titles will be injected into this template
-      title: 'Játék'
-    }
+      title: 'Játék',
+          meta: [
+      {
+        key: 'og:title',
+        name: 'og:title',
+        content: 'Játék',
+      },
+      {
+        key: 'twitter:title',
+        name: 'twitter:title',
+        content: 'Játék',
+      },
+    ]}
 
 }
 </script>
