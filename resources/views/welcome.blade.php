67fa1d20db7b03b790d7c45c11d63e4d13763c26
Gergo Olah <gergo.olah@possible.com>
2020-06-16 15:43:37 +0200

    meta OG

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Kajla Makett</title>
-
+<meta property="og:title" content="Kajla Makett">
+<meta property="og:url" content="">
+<meta property="og:description" content="">
+<meta property="og:type" content="website">
+<meta property="og:image" content="https://makett.kajla.hu/assets/images/mtu_kajla-3d_kihivas_kv_v5.jpg">
         <!-- Styles -->
         <link rel="stylesheet" href="{{mix('/css/app.css')}}"></link>
 
